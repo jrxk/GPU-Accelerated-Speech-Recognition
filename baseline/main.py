2299af12e23f40e097c705745c0ff4e0ab665fb5
Yuying <yuyzhu@outlook.com>
2020-05-05 19:39:40 -0400

    add baseline experiments

--- baseline/main.py
+++ baseline/main.py
@@ -3,19 +3,29 @@ import torch
 import torch.nn as nn
 from config import *
 import time
+import json
 import os
+import sys
 from model import *
 from ctcdecode import CTCBeamDecoder
 
-if __name__ == '__main__':
-    model = DeepSpeech()
-    decoder = CTCBeamDecoder(['$']*output_size, beam_width=5, blank_id=0, num_processes=os.cpu_count(), log_probs_input=True)
-    
-    batch_size = 3
-    seq_len = 9
-    n_iter = 100
-    
-    inp = torch.ones((batch_size, seq_len, input_size+2*input_size*n_context))
+def run(config):
+    batch_size = config["batch_size"]
+    seq_len = config["seg_len"]
+    n_iter = config["epoch"]
+    input_size = config["input_size"]
+    device = config["device"]
+    num_processes = config["num_processes"]
+    # print("num_processes_cpu: ", os.cpu_count())
+    model = DeepSpeech(config)
+    decoder = CTCBeamDecoder(['$']*output_size, beam_width=5, blank_id=0, num_processes=num_processes, log_probs_input=True)
+
+    # inp = torch.ones((batch_size, seq_len, input_size+2*input_size*n_context))
+    inp = torch.rand((batch_size, seq_len, input_size+2*input_size*n_context))
+    print("inp shape:", inp.shape)
+    model = model.to(device)
+    inp = inp.to(device)
+
 
     start_time = time.perf_counter()
     for i in range(n_iter):
@@ -23,9 +33,22 @@ if __name__ == '__main__':
     end_time = time.perf_counter()
     print("Forward: %f s" % ((end_time-start_time)/n_iter))
 
-    start_time = time.perf_counter()
+    start_time1 = time.perf_counter()
     out = out.transpose(0, 1)
     out_lens = torch.tensor([seq_len for _ in range(batch_size)])
     output, scores, timesteps, out_seq_len = decoder.decode(out, out_lens) # [b, seq_len, vocab_size] -> [b, beam_width, seq_len]
     end_time = time.perf_counter()
-    print("CTC Decode %f s" % (end_time-start_time))
+    print("CTC Decode %f s" % (end_time-start_time1))
+    print("Overall %f s" % (end_time-start_times))
+
+
+if __name__ == '__main__':
+    configs = json.load(open(sys.argv[1]))
+    for config in configs:
+        print("====== config ======")
+        print(config)
+        print("====================")
+        run(config)
+
+
+
--- baseline/model.py
+++ baseline/model.py
@@ -1,24 +1,37 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from config import *
+# from config import *
 
 class DeepSpeech(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
         super(DeepSpeech, self).__init__()
+        batch_size = config["batch_size"]
+        input_size = config["input_size"]
+        n_context = config["n_context"]
+        linear_size = config["linear_size"]
+        self.rnn_hidden_size = config["rnn_hidden_size"]
+        vocab_size = config["vocab_size"]
+
+        hidden_1 = linear_size 
+        hidden_2 = linear_size
+        hidden_5 = linear_size
+        self.hidden_3 = self.rnn_hidden_size
+        self.output_size = vocab_size + 1
+
         self.mlp123 = nn.Sequential(
             nn.Linear(input_size+2*input_size*n_context, hidden_1),
             nn.ReLU(inplace=True),
             nn.Linear(hidden_1, hidden_2),
             nn.ReLU(inplace=True),
-            nn.Linear(hidden_2, hidden_3),
+            nn.Linear(hidden_2, self.hidden_3),
             nn.ReLU(inplace=True),
         )
-        self.rnn = nn.RNN(hidden_3, rnn_hidden_size, num_layers=1, bidirectional=False) # bidir true
+        self.rnn = nn.RNN(self.hidden_3, self.rnn_hidden_size, num_layers=1, bidirectional=False) # bidir true
         self.mlp56 = nn.Sequential(
-            nn.Linear(rnn_hidden_size, hidden_5),
+            nn.Linear(self.rnn_hidden_size, hidden_5),
             nn.ReLU(inplace=True),
-            nn.Linear(hidden_5, output_size),
+            nn.Linear(hidden_5, self.output_size),
         )
     
     def forward(self, x):
@@ -28,9 +41,9 @@ class DeepSpeech(nn.Module):
         x = x.permute(1, 0, 2)
         x = x.reshape(seq_len*batch_size, -1)
         x = self.mlp123(x)
-        x = x.reshape(seq_len, batch_size, hidden_3)
+        x = x.reshape(seq_len, batch_size, self.hidden_3)
         x, _ = self.rnn(x)
-        x = x.reshape(seq_len*batch_size, rnn_hidden_size)
+        x = x.reshape(seq_len*batch_size, self.rnn_hidden_size)
         x = self.mlp56(x)
-        x = x.reshape(seq_len, batch_size, output_size)
+        x = x.reshape(seq_len, batch_size, self.output_size)
         return x.log_softmax(2)
